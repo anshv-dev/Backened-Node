a16c9f21cdc6c43b4ed8aeb738a8c11b84f335a2
Ansh Verma <anshv8166@gmail.com>
2025-07-16 22:46:50 +0530

    Change in db

--- db.js
+++ db.js
@@ -19,3 +19,12 @@ const db=mongoose.connection
 db.on('connected',()=>{
      console.log('Connected to MongoDB server') 
 })
+
+
+db.on('error',(err)=>{
+  console.log('MongoDB connection error:',err);
+})
+
+db.on('disconnected',()=>{
+   console.log('MongoDB disconnected')
+})
