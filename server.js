def5af6bb9cd26bb44b2b10a65031b98d52b9ef2
Ansh Verma <anshv8166@gmail.com>
2025-07-17 18:54:49 +0530

    Added Get and Post route

--- server.js
+++ server.js
@@ -74,6 +74,34 @@ app.get('/person',async(req,res)=>{
       res.status(500).json({error:'Internal Server Error'})
     }
 })
+
+
+//Post route for menuitem
+app.post('/menu',async(req,res)=>{
+     try{
+      const data=req.body
+      const newMenu=new MenuItem(data);
+      const response=await newMenu.save();
+      console.log('data saved')
+      res.status(200).json(response);
+    
+    }catch(err){
+      console.log(err);
+      res.status(500).json({error:'Internal Server Error'})
+    }
+})
+
+//Get Route for menu
+app.get('/menu',async(req,res)=>{
+    try{
+      const data=await MenuItem.find();
+      console.log('data fetched');
+      res.status(200).json(data);
+    }catch(err){
+      console.log(err);
+      res.status(500).json({error:'Internal Server Error'})
+    }
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
